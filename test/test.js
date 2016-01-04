91c765e5c8f9ec487c2ff60800eae692532654f2
Thomas Durieux <durieuxthomas@hotmail.com>
2016-01-04 19:17:54 +0100

    [fix #2] Fix the filter value

--- test/test.js
+++ test/test.js
@@ -4,38 +4,38 @@ var leboncoin = require("../main.js");
 describe('Search', function() {
     it('check url wihout parameters', function(done) {
         var s = new leboncoin.Search();
-        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=&ur=0&it=0", s.getUrl());
+        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=a&ur=0&it=0", s.getUrl());
         done();
     });
 
     it('check url with specified page', function(done) {
         var s = new leboncoin.Search()
                     .setPage(1);
-        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=&ur=0&it=0", s.getUrl());
+        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=a&ur=0&it=0", s.getUrl());
 
         s = new leboncoin.Search({
             page: 1
         });
-        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=&ur=0&it=0", s.getUrl());
+        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=a&ur=0&it=0", s.getUrl());
 
         s = new leboncoin.Search()
                     .setPage(2);
-        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=2&f=&ur=0&it=0", s.getUrl());
+        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=2&f=a&ur=0&it=0", s.getUrl());
 
         s = new leboncoin.Search({
             page: 2
         });
-        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=2&f=&ur=0&it=0", s.getUrl());
+        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=2&f=a&ur=0&it=0", s.getUrl());
 
         s = new leboncoin.Search()
                     .setPage('aa');
-        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=&ur=0&it=0", s.getUrl());
+        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=a&ur=0&it=0", s.getUrl());
 
 
         s = new leboncoin.Search({
             page: 'aa'
         });
-        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=&ur=0&it=0", s.getUrl());
+        assert.equal("http://www.leboncoin.fr/annonces/offres/ile_de_france/occasions?o=1&f=a&ur=0&it=0", s.getUrl());
 
         done();
     });
