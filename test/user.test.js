6dabbcde4b93f5c6fd42255509752e71e938d2d4
tdurieux <durieuxthomas@hotmail.com>
2016-04-07 15:10:30 +0200

    Adds basic user support (login, logout, user data)

---
+++ test/user.test.js
@@ -0,0 +1,33 @@
+const cheerio = require('cheerio');
+const should = require('should');
+
+const user = require("../lib/user.js");
+
+describe('User', function() {
+    this.timeout(50000);
+    
+    describe('login', function() {
+        it('via options', function(done) {
+            
+            var u = new user.User({"email": "test@leeching.net", "password": "123456789a"});
+            u.login().then(function (user) {
+                done();
+            }, function (error) {
+                done(error);
+            });
+        });
+    });
+
+    describe('logout', function() {
+        it('via options', function(done) {
+            var u = new user.User({"email": "test@leeching.net", "password": "123456789a"});
+            u.login().then(function (user) {
+                u.logout().then(function (user) {
+                    done();
+                }, done);
+            }, function (error) {
+                done(error);
+            });
+        });
+    });
+});
