327a790737f77c28274998485914261473bceb73
tdurieux <durieuxthomas@hotmail.com>
2020-05-31 11:12:28 +0200

    improve session management

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 module.exports.Search = require('./lib/search').Search;
 module.exports.Item = require('./lib/item').Item;
+module.exports.Session = require('./lib/session').Session;
+module.exports.User = require('./lib/user').User;
+module.exports.utils = require('./lib/utils');
 module.exports.FILTERS = require('./lib/filters');
